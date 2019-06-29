8d1b9759e818aade0332949cd18cc338d3f9326a
William Pettersson <william@ewpettersson.se>
2019-06-29 19:52:08 +0100

    Add script to record temperatures

---
+++ templogger/templogger.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python3
+"""Read temperature at predefined intervals, and store to a database.
+"""
+
+
+from glob import glob
+from time import sleep
+
+import psycopg2
+
+
+SLEEP_TIME = 60
+"""Sleep time, in seconds."""
+
+
+BASE_DIR = '/sys/bus/w1/devices/'
+"""Base dir for w1 devices"""
+DEVICE_FOLDER = glob(BASE_DIR + '28*')[0]
+"""Glob to find the 1 wire device"""
+DEVICE_FILE = DEVICE_FOLDER + '/w1_slave'
+"""The actual device"""
+
+ZONE = 1
+
+DB = 'db_name'
+"""Database name."""
+USER = 'user'
+"""Database user."""
+PASSWORD = 'password'
+"""Database password."""
+DBHOST = 'localhost'
+"""Database host."""
+
+
+def read_temp():
+    """Reads a temperature, returning it in Celsius."""
+    with open(DEVICE_FILE, "r") as devfile:
+        crc = devfile.readline().split(' ')[-1].rstrip()
+        if crc != "YES":
+            return -1
+        return int(devfile.readline().rsplit('t=')[-1].rstrip())
+
+
+def store_temp(temp):
+    """Store the temperature into a database."""
+    conn = psycopg2.connect(dbname=DB, user=USER, password=PASSWORD,
+                            host=DBHOST)
+    cursor = conn.cursor()
+    cursor.execute("INSERT INTO temperatures (zone_id, temp)"
+                   "VALUES (%s, %s)", (ZONE, temp))
+    conn.commit()
+    cursor.close()
+    conn.close()
+
+
+def main():
+    """Main loop"""
+    while True:
+        temp = read_temp()
+        # Don't store -1, which indicates sensor not ready.
+        if temp != -1:
+            store_temp(temp)
+        sleep(SLEEP_TIME)
+
+
+if __name__ == "__main__":
+    main()
