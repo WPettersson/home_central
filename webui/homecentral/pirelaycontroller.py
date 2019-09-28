fd90085cfdc2cc5fdc521ef3ccc88e6cedd5549a
William Pettersson <william@ewpettersson.se>
2019-09-28 20:59:43 +0100

    PiRelayController: store pin in class

--- webui/homecentral/pirelaycontroller.py
+++ webui/homecentral/pirelaycontroller.py
@@ -1,30 +1,26 @@
 """A relay controller class."""
 
 
-from os import environ
 import RPi.GPIO as GPIO
 
 from homecentral.controller import Controller
-
-PIN = 2
-if "HC_RELAY_PIN" in environ:
-    PIN = environ["HC_RELAY_PIN"]
 
 
 class PiRelayController(Controller):
     """Controls a relay
     """
-    def __init__(self, ident):
+    def __init__(self, ident, pin):
         super().__init__(ident)
+        self._pin = pin
         # Don't keep warning about mode if it's already set
         GPIO.setwarnings(False)
         GPIO.setmode(GPIO.BCM)
-        GPIO.setup(PIN, GPIO.OUT)
+        GPIO.setup(self._pin, GPIO.OUT)
 
     def trigger(self, switch_on):
         """If switch_on is True, switch the thing on, else switch it off.
         """
         if switch_on:
-            GPIO.output(PIN, 0)
+            GPIO.output(self._pin, 0)
         else:
-            GPIO.output(PIN, 1)
+            GPIO.output(self._pin, 1)
